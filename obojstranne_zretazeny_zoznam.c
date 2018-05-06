604eed4d754a0ea11ccff123bdc655bfd3046531
horvathova <nikoleta638@gmail.com>
2018-05-06 15:03:57 +0200

    Update obojstranne_zretazeny_zoznam.c

--- obojstranne_zretazeny_zoznam.c
+++ obojstranne_zretazeny_zoznam.c
@@ -4,224 +4,213 @@
 
 #define OK 1
 #define FAIL 0
-#define RANGE 10
-#define RAND_MIN 57
 
-typedef struct TWN{
+typedef struct TWN {
 	float data;
 	struct TWN *prev;
 	struct TWN *next;
 }TWN;
 
-typedef struct CTWL{
+typedef struct CTWL {
 	TWN *cur;
 }CTWL;
 
-void ctwl_cur_step_right(CTWL *list){
-	if(list->cur==NULL){
-		break;
+void ctwl_cur_step_right(CTWL *list) {
+	if (list->cur == NULL) {
+		return;
 	}
-	
-    list->cur=list->cur->next;
+
+	list->cur = list->cur->next;
 }
 
-void ctwl_cur_step_left(CTWL *list){
-	if(list->cur==NULL){
-		break;
+void ctwl_cur_step_left(CTWL *list) {
+	if (list->cur == NULL) {
+		return;
 	}
-	
-	list->cur=list->cur->prev;
+
+	list->cur = list->cur->prev;
 }
 
-TWN *ctwl_insert_right(CTWL* list, float val){
-	TWN *novy, *prvy, *druhy;
-	
-	if(list->cur==NULL){
-		return NULL;
+TWN *ctwl_insert_right(CTWL* list, float val) {
+	TWN *cur, *novy, *dalsi;
+
+	novy = malloc(sizeof(TWN));
+
+	if (novy == NULL) {
+		return 0;
 	}
-	
-	novy=malloc(sizeof(TWN));
-	if(novy==NULL){
-		return NULL;
+
+	novy->data = val;
+
+	if (list->cur == NULL) {
+		list->cur = novy;
+		list->cur->next = novy;
+		list->cur->prev = novy;
+
+		return novy;
 	}
-	
-	prvy=list->cur;
-	druhy=list->cur->next;
-	
-	novy->prev=prvy;
-	novy->next=druhy;
-	druhy->prev=novy;
-	prvy->next=novy;
-	novy->data=val;
-	
+	else {
+		cur = list->cur;
+		dalsi = list->cur->next;
+
+		novy->prev = cur;
+		novy->next = dalsi;
+		list->cur->next = novy;
+		dalsi->prev = novy;
+
+		ctwl_cur_step_right(list);
+		return novy;
+	}
+}
+
+TWN *ctwl_insert_left(CTWL* list, float val) {
+	TWN *cur, *novy, *predosly;
+
+	novy = malloc(sizeof(TWN));
+
+	if (novy == NULL) {
+		return 0;
+	}
+
+	novy->data = val;
+
+	if (list->cur == NULL) {
+		list->cur = novy;
+		list->cur->next = novy;
+		list->cur->prev = novy;
+	}
+	else {
+		cur = list->cur;
+		predosly = list->cur->prev;
+
+		novy->next = cur;
+		novy->prev = predosly;
+		predosly->next = novy;
+		cur->prev = novy;
+
+		ctwl_cur_step_left(list);
+	}
+
 	return novy;
 }
 
-TWN *ctwl_insert_left(CTWL* list, float val){
-	TWN *novy, *prvy, *druhy;
-	
-	if(list->cur==NULL){
-		return NULL;
-	}
-	
-	novy=malloc(sizeof(TWN));
-	if(novy==NULL){
-		return NULL;
-	}
-	
-	druhy= list->cur;
-	prvy=list->cur->next;
-	
-	novy->prev=prvy;
-	novy->next=druhy;
-	druhy->prev=novy;
-	prvy->next=novy;
-	novy->data=val;
-	
-	return novy;
-}
-
-char ctwl_delete(CTWL *list){
+char ctwl_delete(CTWL *list) {
 	TWN *cur, *prev, *next;
-	
-	if(list->cur==NULL){
+
+	if (list->cur == NULL) {
 		return FAIL;
 	}
-	
-	if(list->cur->next == list->cur && list->cur->prev == list->cur){ 
+
+	if (list->cur->next == list->cur && list->cur->prev == list->cur) {
 		free(list->cur->next);
 		free(list->cur->prev);
 		list->cur = NULL;
 		return OK;
-}
-	
-	cur=list->cur;
-	next=list->cur->next;
-	prev=list->cur->prev;
-	
+	}
+
+	cur = list->cur;
+	next = list->cur->next;
+	prev = list->cur->prev;
+
 	ctwl_cur_step_right(list);
 	list->cur->prev = prev;
 	ctwl_cur_step_left(list);
 	list->cur->next = next;
 	ctwl_cur_step_right(list);
-	
+
 	free(cur);
 	return OK;
 }
 
-CTWL *ctwl_create_empty(void){
+CTWL *ctwl_create_empty(void) {
 	CTWL *list;
-	
-	if(list->cur==NULL){
+
+	if ((list = (CTWL*)malloc(sizeof(CTWL))) == NULL) {
 		return NULL;
 	}
-	
-	if((list=(CTWL*)malloc(sizeof(CTWL)))==NULL){
-		return NULL;
-	}
-        
-	list->cur=NULL;
-	list->cur->next=NULL;
-	
+
+	list->cur = NULL;
 	return list;
 }
 
-CTWL *ctwl_create_random(unsigned int size){
-	CTWL *list;
-	TWN *start, *prev, *next;
+CTWL *ctwl_create_random(unsigned int size) {
 	int i;
+	float data;
+	CTWL *list;
 
-	if(list->cur==NULL){
-		return NULL;
+	list = ctwl_create_empty();
+
+	for (i = 0; i<size; i++) {
+		data = rand() % 20;
+		list->cur = ctwl_insert_right(list, data);
 	}
-	
-	list=ctwl_create_empty();
-	list->cur->data=RANGE*(float)rand()/((float)RAND_MAX)+RAND_MIN;
-	start=list->cur;
-	
-	for(i=0;i<size-1;i++){
-		list->cur->next=(TWN*)malloc(sizeof(TWN));
-        if(list->cur->next==NULL){
-            return NULL;
-        }
-	
-        prev=list->cur;
-        ctwl_cur_step_right(list);
-        list->cur->prev=prev;
- 		list->cur->data=RANGE*(float)rand()/((float)RAND_MAX)+RAND_MIN;
-	}
-	
-	list->cur->next=start;
-	prev=list->cur;
-	ctwl_cur_step_right(list);
-	list->cur->prev=prev;
-	
 	return list;
 }
 
-void ctwl_destroy(CTWL *list){
+void ctwl_destroy(CTWL *list) {
 	TWN *cur;
-	
-	if(list->cur==NULL){
-		break;
+
+	if (list->cur == NULL) {
+		return;
 	}
-	
-	cur=list->cur;
-	while(list->cur->next==NULL){
+
+	cur = list->cur;
+	while (list->cur->next == NULL) {
 		ctwl_cur_step_right(list);
-		if(list->cur != prvy){
+		if (list->cur != cur) {
 			free(cur);
 		}
-		
-		else{
-			break
+
+		else {
+			return;
 		}
-			
+
 		free(cur);
 	}
 
 	free(list);
 }
 
-void ctwl_print(CTWL *list){
+void ctwl_print(CTWL *list) {
 	TWN *cur;
-	
-	if(list->cur==NULL){
-		break;
+
+	if (list->cur == NULL) {
+		return;
 	}
-	
-	cur=list->cur;
+
+	cur = list->cur;
+
 	printf("%.3f ", list->cur->data);
 	ctwl_cur_step_right(list);
 
-	while(cur != list->cur){
-		printf("%.3f ",list->cur->data);
+	while (list->cur != cur) {
+		printf("%.3f ", list->cur->data);
 		ctwl_cur_step_right(list);
 	}
 }
 
-float ctwl_sum_values(CTWL *list){
+float ctwl_sum_values(CTWL *list) {
 	TWN *cur;
 	float sum;
-	
-	if(list->cur==NULL){
-		break;
+
+	if (list->cur == NULL) {
+		return 0;
 	}
-	
-	sum=0;
-	cur=list->cur;
-	sum=sum+list->cur->data;
+
+	sum = 0;
+	cur = list->cur;
+	sum = sum + list->cur->data;
 	ctwl_cur_step_right(list);
 	
-	while(cur!=list->cur){
-		sum=sum+cur->data;
+	while (cur != list->cur) {
+		sum = sum + list->cur->data;
 		ctwl_cur_step_right(list);
 	}
-	
+
 	return sum;
 }
 
-main(){
+int main() {
 	unsigned int size;
 	float sum;
 	CTWL *list;
@@ -229,12 +218,16 @@ main(){
 
 	srand(time(NULL));
 	printf("Zadaj pocet prvkov:");
-	scanf("%d",&size);
+	scanf("%d", &size);
 
-	list=ctwl_create_random(size);
+	list = ctwl_create_random(size);
 	printf("Zoznam :\n");
 	ctwl_print(list);
-	sum=ctwl_sum_values(list);
-	printf("\nSum :%.3f\n",sum);
 
+	sum = ctwl_sum_values(list);
+	printf("\nSum :%.3f\n", sum);
+	ctwl_destroy(list);
+
+	getchar();
+	getchar();
 }
